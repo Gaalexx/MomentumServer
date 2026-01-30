164bfd527e8ae36125e472aa60611be599143bd5
Gaalexx <gaalex05@gmail.com>
2026-01-30 14:11:45 +0300

    Init repo

---
+++ MomentumServer/src/main/kotlin/Monitoring.kt
@@ -0,0 +1,35 @@
+package com.example
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.codahale.metrics.*
+import io.ktor.http.*
+import io.ktor.openapi.OpenApiInfo
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.metrics.dropwizard.*
+import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.plugins.openapi.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.util.concurrent.TimeUnit
+import org.slf4j.event.*
+
+fun Application.configureMonitoring() {
+    install(DropwizardMetrics) {
+        Slf4jReporter.forRegistry(registry)
+            .outputTo(this@configureMonitoring.log)
+            .convertRatesTo(TimeUnit.SECONDS)
+            .convertDurationsTo(TimeUnit.MILLISECONDS)
+            .build()
+            .start(10, TimeUnit.SECONDS)
+    }
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+}
---
+++ MomentumServer/src/main/kotlin/Serialization.kt
@@ -0,0 +1,23 @@
+package com.example
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.codahale.metrics.*
+import io.ktor.http.*
+import io.ktor.openapi.OpenApiInfo
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.metrics.dropwizard.*
+import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.plugins.openapi.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.util.concurrent.TimeUnit
+import org.slf4j.event.*
+
+fun Application.configureSerialization() {
+}
