168ca0241b765443bbfaf58818f72e279c1d4802
guse95 <saveliy.gusev.05@list.ru>
2026-03-20 19:01:07 +0300

    change file for CheckUserInfoIsFreeRequestDTO, CheckUserInfoIsFreeResponseDTO to AccountDTO

--- MomentumServer/src/main/kotlin/Models/AccountDTO.kt
+++ MomentumServer/src/main/kotlin/Models/AccountDTO.kt
@@ -15,3 +15,17 @@ data class EditAccountDTO (
     val phone: String?,
     val profilePhotoURL: String?
 )
+
+@Serializable
+data class CheckUserInfoIsFreeRequestDTO(
+    val username: String?,
+    val email: String?,
+    val phone: String?,
+)
+
+@Serializable
+data class CheckUserInfoIsFreeResponseDTO(
+    val isUsernameFree: Boolean?,
+    val isEmailFree: Boolean?,
+    val isPhoneFree: Boolean?,
+)
