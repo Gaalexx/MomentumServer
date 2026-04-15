887cbf5b5951ccc22dea5c79e0409fd57666aa3c
Gaalexx <gaalex05@gmail.com>
2026-04-15 23:44:41 +0300

    Reapply "added type for file"
    This reverts commit 7279d490f6b9e5d5a57c4a38bad64509927cbda6.

--- MomentumServer/src/main/kotlin/routing/S3InteractionRouting.kt
+++ MomentumServer/src/main/kotlin/routing/S3InteractionRouting.kt
@@ -131,11 +131,11 @@ fun Route.s3Routes(jwtService: JwtService){ // TODO доделать удале�
             listOfFriends.forEach { friend ->
                 val posts = PostsTable.getPostsOfUser(UUID.fromString(friend.userId))
                 posts.forEach { post ->
-                    val media = MediaTable.getObjectKeyOfPost(post.mediaId)
+                    val media = MediaTable.getMediaById(post.mediaId)
                     if(media != null){
-                        val presignedURL = S3Client.getPresignedObjectUrl(media)
+                        val presignedURL = S3Client.getPresignedObjectUrl(media.objectKey)
                         listToSend.add(PostDTO(post.id.toString(), post.userId.toString(), userName = friend.username,
-                            title = post.title, inUse = post.inUse, presignedURL = presignedURL, avatarPresignedURL = friend.userAvatarUrl, createdAt = post.createdAt))
+                            title = post.title, inUse = post.inUse, presignedURL = presignedURL, avatarPresignedURL = friend.userAvatarUrl, createdAt = post.createdAt, mediaType = media.mediaType,))
                     }
                 }
             }
@@ -156,10 +156,10 @@ fun Route.s3Routes(jwtService: JwtService){ // TODO доделать удале�
             val listToSend: MutableList<PostDTO> = mutableListOf()
             if(user != null){
                 listOfPosts.forEach { it ->
-                    val media = MediaTable.getObjectKeyOfPost(it.mediaId)
+                    val media = MediaTable.getMediaById(it.mediaId)
                     if(media != null){
-                        val presignedURL = S3Client.getPresignedObjectUrl(media)
-                        listToSend.add(PostDTO(it.id.toString(), it.userId.toString(), userName = user.username ?: user.email, it.title, it.inUse, presignedURL, presignedAvatarURL, it.createdAt))
+                        val presignedURL = S3Client.getPresignedObjectUrl(media.objectKey)
+                        listToSend.add(PostDTO(it.id.toString(), it.userId.toString(), userName = user.username ?: user.email, it.title, it.inUse, presignedURL, avatarPresignedURL = presignedAvatarURL, createdAt = it.createdAt, mediaType = media.mediaType,))
                     }
                 }
             }
