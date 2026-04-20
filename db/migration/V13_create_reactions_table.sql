f586998274bc62fc197dddb3a0ea29f1674d7376
guse95 <saveliy.gusev.05@list.ru>
2026-04-21 01:06:43 +0300

    bug fix (",")

--- db/migration/V13_create_reactions_table.sql
+++ db/migration/V13_create_reactions_table.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS public.reactions
 
     CONSTRAINT reactions_pkey PRIMARY KEY (id),
     CONSTRAINT reactions_user_id_fk FOREIGN KEY (user_id)
-    REFERENCES public.users (id)
+    REFERENCES public.users (id),
     CONSTRAINT reactions_post_id_fk FOREIGN KEY (post_id)
     REFERENCES public.posts (id)
     ON UPDATE NO ACTION
