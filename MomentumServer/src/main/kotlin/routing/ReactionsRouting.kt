bae481c95b6f59f6d051b4a8e6dedf263a88b6f8
guse95 <saveliy.gusev.05@list.ru>
2026-04-22 17:09:16 +0300

    rename routing

--- MomentumServer/src/main/kotlin/routing/ReactionsRouting.kt
+++ MomentumServer/src/main/kotlin/routing/ReactionsRouting.kt
@@ -1,27 +1,19 @@
 package com.example.routing
 
-import com.example.Models.PresignedURLDTO
 import com.example.Models.ReactionsModel
-import com.example.Models.UploadInfoDTO
-import com.example.database.MediaModel
-import com.example.database.MediaTable
 import com.example.database.ReactionsTable
-import com.example.s3Client.S3Client
 import com.example.tokens.JwtService
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.auth.authenticate
 import io.ktor.server.auth.jwt.JWTPrincipal
 import io.ktor.server.auth.principal
-import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.delete
-import io.ktor.server.routing.get
 import io.ktor.server.routing.post
-import java.time.Duration
 import java.util.UUID
 
-fun Route.ReactionsRootes(jwtService: JwtService){
+fun Route.reactionsRoutes(jwtService: JwtService){
 
     authenticate("jwt"){
         post("/react/{post_id}/{reaction_type}") {
