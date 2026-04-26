17a27fd4167564923519ee6a76fa5eace10a1536
Gaalexx <gaalex05@gmail.com>
2026-04-26 21:53:50 +0300

    fix

--- MomentumServer/src/main/kotlin/firebase/InitFirebase.kt
+++ MomentumServer/src/main/kotlin/firebase/InitFirebase.kt
@@ -3,16 +3,19 @@ package com.example.firebase
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
+import java.io.FileInputStream
 
 fun initFirebase() {
-    val serviceAccount = object {}.javaClass
-        .getResourceAsStream("/firebase-service-account.json")
+    if (FirebaseApp.getApps().isNotEmpty()) return
 
-    val options = FirebaseOptions.builder()
-        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-        .build()
+    val credentialsPath = System.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+        ?: error("GOOGLE_APPLICATION_CREDENTIALS is not set")
 
-    if (FirebaseApp.getApps().isEmpty()) {
+    FileInputStream(credentialsPath).use { serviceAccount ->
+        val options = FirebaseOptions.builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .build()
+
         FirebaseApp.initializeApp(options)
     }
 }
