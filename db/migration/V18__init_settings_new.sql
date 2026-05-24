8c3d2b8e4a04df84055d57bf66f1caf0d2e6144b
AlekseiZavorotnyi <zavorotny2005@gmail.com>
2026-05-25 00:28:54 +0300

    rebrand settings finished

--- db/migration/V18__init_settings_new.sql
+++ db/migration/V18__init_settings_new.sql
@@ -1,8 +1,12 @@
 ALTER TABLE public.settings
     RENAME COLUMN recommend_to_contacts TO friend_request_enabled;
+
+ALTER TABLE public.settings
     RENAME COLUMN allow_add_from_anyone TO default_theme_enabled;
+
+ALTER TABLE public.settings
     ALTER COLUMN in_app_notifications SET DEFAULT TRUE,
-    ALTER COLUMN publications_enabled SET DEFAULT TRUE,
+ALTER COLUMN publications_enabled SET DEFAULT TRUE,
     ALTER COLUMN reactions_enabled SET DEFAULT TRUE,
     ALTER COLUMN friend_request_enabled SET DEFAULT TRUE,
     ALTER COLUMN default_theme_enabled SET DEFAULT TRUE;
